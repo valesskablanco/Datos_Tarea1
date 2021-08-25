6034b9def3b16900ede5083bd0916a2736877b1f
valesskablanco <valesskablanco2@gmail.com>
2021-08-24 21:10:25 -0600

    Avances Client

--- scr/Client.java
+++ scr/Client.java
@@ -1,5 +1,27 @@
 package scr;
 
-public class Client {
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Client implements Runnable {
+    
+    private String send;
+    private String port;
+
+    public Client(String port, String send) {
+
+        this.send = send;
+        this.port = port;
+    }
+    
+    @Override
+
+    public void run() {
+
+        // ahí vamos
+    }
     
 }
