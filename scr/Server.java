54a85c335a706c20e2c4299d2b0269701a86acd1
valesskablanco <valesskablanco2@gmail.com>
2021-08-25 17:28:49 -0600

    Inicializando el servidor

--- scr/Server.java
+++ scr/Server.java
@@ -1,5 +1,36 @@
 package scr;
 
-public class Server {
+import java.io.DataInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.xml.crypto.Data;
+
+import java.util.StringTokenizer;
+
+public class Server implements Runnable {
+    
+    static int port = 5000;
+    static int myport;
+    private int mipuerto;
+
+    @Override
+    public void run() {
+
+        ServerSocket servidor = null;
+        Socket clientSocket = null;
+        DataInputStream inputStream;
+
+        try {
+
+            //suave
+        } catch {
+            
+            //
+        }
+    }
     
 }
