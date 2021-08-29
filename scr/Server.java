411056bffaaf8a86dd6fd1df5f4b3760990915c7
valesskablanco <valesskablanco2@gmail.com>
2021-08-28 22:46:05 -0600

    Server

--- scr/Server.java
+++ scr/Server.java
@@ -1,60 +1,32 @@
 package scr;
 
-import java.io.DataInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.Observable;
+import java.io.PrintWriter;
 import java.util.StringTokenizer;
 
-@SuppressWarnings("deprecation")
+public class Server {
 
-public class Server extends Observable implements Runnable {
-    
-    private int puerto = 5000;
+    private static final int puerto = 5000;
 
-    @Override
-    public void run() {
+    public static void main(String[] args) throws IOException {
+        
+        ServerSocket server = new ServerSocket(puerto);
 
-        ServerSocket servidor = null;
-        Socket clientSocket = null;
-        DataInputStream inputStream;
+        System.out.println("[SERVER] Esperando conexón...");
+        Socket client = server.accept();
+        System.out.println("[SERVER] Cliente conectado.");
 
-        try {
+        PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+        out.println(obtenerDato("Aquí llega el mensaje"));
 
-            servidor = new ServerSocket(puerto);
-            System.out.print("Servidor Iniciado");
+        server.close();
+        client.close();
 
-            while (true) {
-
-                clientSocket = servidor.accept();
-                System.out.println("Cliente conectado");
-                inputStream = new DataInputStream(clientSocket.getInputStream());
-
-                String datos = inputStream.readUTF();
-                String[] mensaje = separaDatos(datos); //Datos separados en números enteros
-
-                System.out.println(mensaje[0]+mensaje[1]+mensaje[2]);
-
-                this.setChanged();
-                this.notifyObservers(datos);
-                this.clearChanged();
-
-                clientSocket.close();
-                System.out.println("Cliente desconectado");
-
-            }
-        } catch (IOException ex) {
-
-            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
-
-            //Handle exception
-        }
     }
-    
-    public static String[] separaDatos(String datos) {
+
+    public static double obtenerDato(String datos) {
 
         StringTokenizer tokens = new StringTokenizer(datos, ".");
         String[] datosArray = new String[tokens.countTokens()];
@@ -65,9 +37,15 @@ public class Server extends Observable implements Runnable {
             String num = tokens.nextToken();
             datosArray[i] = num;
             i++;
-
         }
-        return datosArray;
+
+        double valor = Integer.parseInt(datosArray[0]);
+        double porcentaje = Integer.parseInt(datosArray[1]);
+        double peso = Integer.parseInt(datosArray[2]);
+
+        double monto = (valor * porcentaje / 100) + (peso * 0.15);
+
+        return monto;
 
     }
 
